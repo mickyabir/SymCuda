77ab89f55f7a2ba7301ac8a3da3be31966af1673
Micky Abir <abir@cs.ucla.edu>
2022-05-09 18:53:08 +0000

    adds u3 gate

--- main.cu
+++ main.cu
@@ -142,6 +142,52 @@ Matrix generateRz(const char * theta_name)
   return Matrix(2, 2, elements);
 }
 
+Matrix generateU3(const char * theta_name, const char * phi_name, const char * lambda_name)
+{
+  SymNode ** elements = new SymNode*[4];
+
+  Symbol * theta0 = new Symbol(theta_name);
+  SymComplex * two0 = new SymComplex(2, 0);
+  SymDiv * div0 = new SymDiv(theta0, two0);
+  elements[0] = new SymCos(div0);
+
+  SymComplex * i1 = new SymComplex(0, 1);
+  Symbol * lambda1 = new Symbol(lambda_name);
+  SymMul * mul10 = new SymMul(i1, lambda1);
+  SymExp * exp1 = new SymExp(mul10);
+  SymComplex * neg1 = new SymComplex(-1, 0);
+  SymMul * mul11 = new SymMul(neg1, exp1);
+  Symbol * theta1 = new Symbol(theta_name);
+  SymComplex * two1 = new SymComplex(2, 0);
+  SymDiv * div1 = new SymDiv(theta1, two1);
+  SymSin * sin1 = new SymSin(div1);
+  elements[1] = new SymMul(mul11, sin1);
+
+  SymComplex * i2 = new SymComplex(0, 1);
+  Symbol * phi2 = new Symbol(phi_name);
+  SymMul * mul20 = new SymMul(i2, phi2);
+  SymExp * exp2 = new SymExp(mul20);
+  Symbol * theta2 = new Symbol(theta_name);
+  SymComplex * two2 = new SymComplex(2, 0);
+  SymDiv * div2 = new SymDiv(theta2, two2);
+  SymSin * sin2 = new SymSin(div2);
+  elements[2] = new SymMul(exp2, sin2);
+
+  SymComplex * i3 = new SymComplex(0, 1);
+  Symbol * phi3 = new Symbol(phi_name);
+  Symbol * lambda3 = new Symbol(lambda_name);
+  SymAdd * add3 = new SymAdd(phi3, lambda3);
+  SymMul * mul30 = new SymMul(i3, add3);
+  SymExp * exp3 = new SymExp(mul30);
+  Symbol * theta3 = new Symbol(theta_name);
+  SymComplex * two3 = new SymComplex(2, 0);
+  SymDiv * div3 = new SymDiv(theta3, two3);
+  SymCos * cos3 = new SymCos(div3);
+  elements[3] = new SymMul(exp3, cos3);
+
+  return Matrix(2, 2, elements);
+}
+
 int main(int argc, char const *argv[])
 {
   SymNode ** elemA = new SymNode*[1];
@@ -164,11 +210,11 @@ int main(int argc, char const *argv[])
   delete[] concrete;
 
   int n = 2;
-  Matrix rz = generateRz("theta");
+  Matrix u3 = generateU3("theta", "phi", "lambda");
 
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
-      rz[i * n + j]->print();
+      u3[i * n + j]->print();
       printf(" | ");
     }
     printf("-------------------------------------------------------------\n");
